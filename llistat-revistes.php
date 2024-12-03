ab2af75c0efd0aab9e7d935022a8213a9d226ee9
Enric Pineda <hola@enricpineda.cat>
2024-12-03 11:38:37 +0100

    v1.7.1

--- llistat-revistes.php
+++ llistat-revistes.php
@@ -3,9 +3,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include_once 'includes/functions.inc';
-$query = "select nom,url from revistes where (paquet like 'fbjl' or paquet like 'cyclops') order by revistes.nom asc";
+$query = "select nom,url,paquet from revistes where (paquet like 'fbjl' or paquet like 'cyclops') order by revistes.nom asc";
 $result = consultaBasedeDades($query);
 while ($fila = mysqli_fetch_row($result)) {
-    echo "<a href=\"".$fila[1]."\" target=\"_blank\">".$fila[0]."</a><br>";
+    echo $fila[2]." | <a href=\"".$fila[1]."\" target=\"_blank\">".$fila[0]."</a><br>";
 }
 ?>
