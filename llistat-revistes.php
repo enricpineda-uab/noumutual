687d587a324c189f59154f93fb2becc8c079db4f
Enric Pineda <hola@enricpineda.cat>
2024-12-03 11:37:14 +0100

    Error en equal

--- llistat-revistes.php
+++ llistat-revistes.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 include_once 'includes/functions.inc';
 $query = "select nom,url from revistes where (paquet like 'fbjl' or paquet like 'cyclops') order by revistes.nom asc";
 $result = consultaBasedeDades($query);
-while ($fila == mysqli_fetch_row($result)) {
+while ($fila = mysqli_fetch_row($result)) {
     echo "<a href=\"".$fila[1]."\" target=\"_blank\">".$fila[0]."</a><br>";
 }
 ?>
