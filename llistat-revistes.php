965033c33f443d071cc02eb1fc1506d0f5554c57
Enric Pineda <hola@enricpineda.cat>
2024-12-03 11:29:33 +0100

    Solucionat error en nom de funció

--- llistat-revistes.php
+++ llistat-revistes.php
@@ -2,7 +2,7 @@
 include_once 'includes/functions.inc';
 $query = "select revistes.nom,revistes.url,revistes.embargo,revistes.paquet,revistes.username,revistes.passwd,revistes.id from revistes,rev2pat where (revistes.paquet like 'fbjl' or revistes.paquet like 'cyclops') and order by revistes.nom asc";
 $result = consultaBasedeDades($query);
-while ($fila = msqli_fetch_row($result)) {
+while ($fila = mysqli_fetch_row($result)) {
     echo "<a href=\"".$fila[1]."\" target=\"_blank\">".$fila[0]."</a><br>";
 }
 ?>
